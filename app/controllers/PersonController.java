c6c550b918e5be9ddacd8e47ce9b67fcd49d9cb9
Ender Aydın Orak <eaorak@gmail.com>
2014-02-16 10:02:48 +0200

    x

--- app/controllers/PersonController.java
+++ app/controllers/PersonController.java
@@ -1,6 +1,10 @@
 package controllers;
 
-import play.mvc.Before;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Location;
+import models.Person;
 import play.mvc.Controller;
 
 public class PersonController extends Controller {
@@ -9,9 +13,36 @@ public class PersonController extends Controller {
 		render();
 	}
 
-	@Before
-	public static void init() {
+	private static List<Person> persons = new ArrayList<Person>();
 
+	static {
+		Location loc = new Location();
+		loc.lat = 41.0933911f;
+		loc.lon = 29.0045748f;
+
+		Person p = new Person();
+		p.id = 1;
+		p.name = "Loris Medici";
+		p.age = 38;
+		p.address = "Istanbul";
+		p.loc = loc;
+		persons.add(p);
+
+		p = new Person();
+		p.id = 2;
+		p.name = "Demet Akyildirim";
+		p.age = 25;
+		p.address = "Istanbul";
+		p.loc = loc;
+		persons.add(p);
+
+		p = new Person();
+		p.id = 3;
+		p.name = "Ender Aydin Orak";
+		p.age = 33;
+		p.address = "Istanbul";
+		p.loc = loc;
+		persons.add(p);
 	}
 
 	public static void create() {
@@ -21,7 +52,10 @@ public class PersonController extends Controller {
 	public static void get() {
 		String id = params.get("id");
 		if (id == null) {
-
+			renderJSON(persons);
+		} else {
+			int i = Integer.parseInt(id) - 1;
+			renderJSON(persons.get(i));
 		}
 	}
 
